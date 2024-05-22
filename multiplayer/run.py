3a653ccfa1fb764e155e5358f988907e759df957
Artyom Kobzar <a.kobzar1995@gmail.com>
2024-05-22 19:22:09 +0500

    Refactor display selection in multiplayer

--- multiplayer/run.py
+++ multiplayer/run.py
@@ -32,7 +32,8 @@ def multiplayer(settings: Settings, screen: pygame.Surface) -> None:
     # Initialize screen, game settings, statistics, scoreboard, fps limiter
     pygame.init()
     stats = GameStats(settings)
-    screen = pygame.display.set_mode((resolution[0], resolution[1]), pygame.FULLSCREEN)
+    display = pygame.display.get_desktop_sizes().index(resolution)
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, display)
 
     pygame.display.set_caption("Ping Pong Game")
     clock = pygame.time.Clock()
