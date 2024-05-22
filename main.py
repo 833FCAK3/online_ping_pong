87b9a9e4aca9095fa7105cf2f760636095a14ba5
Artyom Kobzar <a.kobzar1995@gmail.com>
2024-05-22 14:21:44 +0500

    Sketch out main menu

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+import sys
+
+import pygame
+
+from game_stats import GameStats
+from menu import BaseButton
 from multiplayer.run import multiplayer
 from singleplayer.run import single_player
 from two_players.run import two_players
 
 
-if __name__ == "__main__":
-    multiplayer()
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, display=0)
+
+
+clock = pygame.time.Clock()
+single_player_button = BaseButton(screen, "Single Player", -100)
+two_players_button = BaseButton(screen, "Two Players")
+multiplayer_button = BaseButton(screen, "Multiplayer", 100)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_q:
+                    sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            # check mouse click
+
+    single_player_button.render()
+    two_players_button.render()
+    multiplayer_button.render()
+
+    pygame.display.flip()
+
+    clock.tick(30)
--- menu.py
+++ menu.py
@@ -6,14 +6,13 @@ import pygame.freetype
 from game_stats import GameStats
 
 
-class Button:
-    def __init__(self, screen: pygame.Surface, stats: GameStats, msg: str, alt_msg: str = "") -> None:
-        self.stats = stats
+class BaseButton:
+    def __init__(self, screen: pygame.Surface, msg: str, offset: int = 0) -> None:
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
         # Dimensions and properties of the button
-        self.width, self.height = 220, 60
+        self.width, self.height = 320, 65
         self.button_colour = (0, 200, 0)
         self.text_colour = (255, 255, 255)
 
@@ -22,12 +21,25 @@ class Button:
         # Build the buttons's rect object, and center it
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
+        self.rect.centery += offset
 
         self.msg = msg
-        self.alt_msg = alt_msg
-
         self.msg_rect = self.font.render(self.msg, self.text_colour)[1]
-        self.msg_rect.center = self.screen_rect.center
+        self.msg_rect.center = self.rect.center
+
+    def render(self) -> None:
+        """Draw the button on the screen"""
+        self.screen.fill(self.button_colour, self.rect)
+        self.font.render_to(self.screen, self.msg_rect, self.msg, self.text_colour)
+
+
+class Button(BaseButton):
+    def __init__(self, screen: pygame.Surface, stats: GameStats, msg: str, alt_msg: str = "") -> None:
+        super(Button, self).__init__(screen, msg)
+        self.stats = stats
+
+        self.alt_msg = alt_msg if alt_msg else self.msg
+
         self.alt_msg_rect = self.font.render(self.alt_msg, self.text_colour)[1]
         self.alt_msg_rect.center = self.screen_rect.center
 
